8ce36cc21e80302c8bdbbd156a1629b739aa020e
Justin Thomas <justin@justinthomas.name>
2012-05-10 23:13:28 -0700

    add user list to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(:first, :conditions => [ "uuid = ?", params[:uuid]])
     @message = Message.new
+    @users = User.all
 
     respond_to do |format|
       format.html # show.html.erb
